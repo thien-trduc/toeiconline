e97508756b77536828f9d98d36ab3a7b1d8ea2e3
thien <tntran496@gmail.com>
2020-01-02 13:40:40 +0700

    create service

---
+++ toeic-core-dao-impl/src/main/java/vn/myclass/core/daoimpl/UserDaoImpl.java
@@ -0,0 +1,8 @@
+package vn.myclass.core.daoimpl;
+
+import vn.myclass.core.dao.UserDao;
+import vn.myclass.core.data.daoimpl.AbstractDao;
+import vn.myclass.core.persistence.enity.UserEntity;
+
+public class UserDaoImpl extends AbstractDao<Integer, UserEntity> implements UserDao {
+}
---
+++ toeic-core-dao/src/main/java/vn/myclass/core/dao/UserDao.java
@@ -0,0 +1,8 @@
+package vn.myclass.core.dao;
+
+import vn.myclass.core.data.dao.GenericDao;
+import vn.myclass.core.persistence.enity.UserEntity;
+
+
+public interface UserDao extends GenericDao<Integer, UserEntity> {
+}
